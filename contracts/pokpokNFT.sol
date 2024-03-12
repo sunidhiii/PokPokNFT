0256868ab9de1ada62739046c54c7f354f4cb0eb
unknown <maheshwaran@ryuklabs.io>
2024-03-12 18:11:00 +0530

    RSN-854 : hashRoot whitelist

--- contracts/pokpokNFT.sol
+++ contracts/pokpokNFT.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+/ SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -17,18 +17,24 @@ contract pokpok is
     ERC2981,
     Ownable
 {
+    uint256 public MAX_SUPPLY = 888;
     uint256 private _tokenIdCounter;
-    bytes32 public whitelistRoot;
+    bytes32 public whitelistRoot1;
+    bytes32 public whitelistRoot2;
     string private baseTokenURI;
+    
+    event Claimed(address indexed claimer, uint256 indexed tokenId);
     
     constructor(
         string memory name,
         string memory symbol,
         string memory _baseTokenURI,
-        bytes32 root
+        bytes32 root1,
+        bytes32 root2
     ) Ownable(msg.sender) ERC721(name, symbol) {
         baseTokenURI = _baseTokenURI;
-        whitelistRoot = root;
+        whitelistRoot1 = root1;
+        whitelistRoot2 = root2;
     }
 
    function _update(address to, uint256 tokenId, address auth)
@@ -46,8 +52,14 @@ contract pokpok is
         super._increaseBalance(account, value);
     }
 
+
     function baseURI() external view returns (string memory) {
         return _baseURI();
+    }
+    
+    function setWhitelistRoot(bytes32 root1,bytes32 root2) external onlyOwner {
+        whitelistRoot1 = root1;
+        whitelistRoot2 = root2;
     }
 
     function setBaseURI(string memory _baseTokenURI) external onlyOwner {
@@ -59,13 +71,15 @@ contract pokpok is
         uint96 _royaltyFee, 
         bytes32[] memory proof
     ) external virtual returns (uint256 _tokenId) {
-        require(MerkleProof.verify(proof, whitelistRoot, bytes32(uint256(uint160(msg.sender)))), "Invalid proof");
+        require(MerkleProof.verify(proof, whitelistRoot1, bytes32(uint256(uint160(msg.sender)))) || MerkleProof.verify(proof, whitelistRoot2, bytes32(uint256(uint160(msg.sender)))), "Invalid proof");
+        require(totalSupply() < MAX_SUPPLY, "All tokens have been minted");
         _tokenId = _tokenIdCounter;
         _mint(_msgSender(), _tokenId);
         _setTokenURI(_tokenId, _tokenURI);
         _setTokenRoyalty(_tokenId, _msgSender(), _royaltyFee);
         _tokenIdCounter += 1;
         return _tokenId;
+        emit Claimed(_msgSender(), _tokenId);
     }
 
     function tokenURI(uint256 tokenId)
